43e87657a36f795baddb775ed5c9e13f7cc2a1d0
Yovanny Rodriguez <yovannyr87@gmail.com>
2022-12-22 12:00:11 -0400

    Tarea del primer modulo

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
 ﻿
+// Escribe un programa que reciba tu nombre y lo escriba por consola.
+string nombre = "Yovanny Rodriguez";
+Console.WriteLine("Mi nombre es: {0} ", nombre);
+
+Console.WriteLine();
+
+// Escribe un programa que tome la hora y la escriba por pantalla.
 string hora = DateTime.Now.ToString("h:mm:ss tt");
 Console.WriteLine("La hora es: {0}", hora);
 
 Console.ReadLine();
+
+
